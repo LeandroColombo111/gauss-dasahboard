ca98d0bf41803489b6593013f8ec72db2555dfcf
LeandroColombo111 <leandronahuelcolombo@gmail.com>
2025-08-20 11:27:35 -0300

    fix: correct vite base for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gauss-dasahboard/', // 👈 mismo nombre del repo en GitHub
+  base: '/gauss-dasahboard/', 
 })
+
